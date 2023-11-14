7603b1ccc8fcf194d4016dcf34aa1dcb322082ee
DESKTOP-1QSRUQV\Familia <dvmguimaraes@gmail.com>
2023-11-14 18:54:34 -0300

    adiciona mais mocks

--- src/mocks/mockCards.js
+++ src/mocks/mockCards.js
@@ -25,7 +25,124 @@ export const mockBlogData = [
         framework: 'Angular',
         date: '2023-11-05',
         slug: 'desenvolvimento-frontend-com-angular',
-        imageSrc: "https://logos-world.net/wp-content/uploads/2023/02/JavaScript-Symbol.png"
+        imageSrc: "https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-transparent-svg-vector-bie-supply-2.png"
     },
+    {
+        title: 'Trabalhando com APIs RESTful',
+        category: 'Technology',
+        technology: 'REST',
+        framework: 'None',
+        date: '2023-11-20',
+        slug: 'trabalhando-com-apis-restful',
+        imageSrc: "https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-transparent-svg-vector-bie-supply-2.png"
+    },
+    {
+        title: 'Construindo Aplicações Nativas com React Native',
+        category: 'Framework',
+        technology: 'JavaScript',
+        framework: 'React Native',
+        date: '2023-11-25',
+        slug: 'construindo-aplicacoes-com-react-native',
+        imageSrc: "https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-transparent-svg-vector-bie-supply-2.png"
+    },
+    {
+        title: 'Explorando o Ecossistema Node.js',
+        category: 'Technology',
+        technology: 'JavaScript',
+        framework: 'Node.js',
+        date: '2023-11-30',
+        slug: 'explorando-ecossistema-nodejs',
+        imageSrc: "https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-transparent-svg-vector-bie-supply-2.png"
+    },
+    {
+        title: 'Introdução ao Desenvolvimento Web Responsivo',
+        category: 'Web Development',
+        technology: 'HTML/CSS',
+        framework: 'None',
+        date: '2023-12-05',
+        slug: 'introducao-web-responsivo',
+        imageSrc: "https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-transparent-svg-vector-bie-supply-2.png"
+    },
+    {
+        title: 'Gerenciando Estado no React com Redux',
+        category: 'Framework',
+        technology: 'JavaScript',
+        framework: 'Redux',
+        date: '2023-12-10',
+        slug: 'gerenciando-estado-react-redux',
+        imageSrc: "https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-transparent-svg-vector-bie-supply-2.png"
+    },
+    {
+        title: 'Introdução ao React',
+        category: 'Language',
+        technology: 'JavaScript',
+        framework: 'React',
+        date: '2023-11-15',
+        slug: 'introducao-ao-react',
+        imageSrc: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1150px-React-icon.svg.png"
+    },
+    {
+        title: 'Manipulação de Dados com SQL',
+        category: 'Language',
+        technology: 'SQL',
+        framework: 'None',
+        date: '2023-11-10',
+        slug: 'manipulacao-de-dados-com-sql',
+        imageSrc: "https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-transparent-svg-vector-bie-supply-2.png"
+    },
+    {
+        title: 'Desenvolvimento Frontend com Angular',
+        category: 'Framework',
+        technology: 'JavaScript',
+        framework: 'Angular',
+        date: '2023-11-05',
+        slug: 'desenvolvimento-frontend-com-angular',
+        imageSrc: "https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-transparent-svg-vector-bie-supply-2.png"
+    },
+    {
+        title: 'Trabalhando com APIs RESTful',
+        category: 'Technology',
+        technology: 'REST',
+        framework: 'None',
+        date: '2023-11-20',
+        slug: 'trabalhando-com-apis-restful',
+        imageSrc: "https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-transparent-svg-vector-bie-supply-2.png"
+    },
+    {
+        title: 'Construindo Aplicações Nativas com React Native',
+        category: 'Framework',
+        technology: 'JavaScript',
+        framework: 'React Native',
+        date: '2023-11-25',
+        slug: 'construindo-aplicacoes-com-react-native',
+        imageSrc: "https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-transparent-svg-vector-bie-supply-2.png"
+    },
+    {
+        title: 'Explorando o Ecossistema Node.js',
+        category: 'Technology',
+        technology: 'JavaScript',
+        framework: 'Node.js',
+        date: '2023-11-30',
+        slug: 'explorando-ecossistema-nodejs',
+        imageSrc: "https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-transparent-svg-vector-bie-supply-2.png"
+    },
+    {
+        title: 'Introdução ao Desenvolvimento Web Responsivo',
+        category: 'Web Development',
+        technology: 'HTML/CSS',
+        framework: 'None',
+        date: '2023-12-05',
+        slug: 'introducao-web-responsivo',
+        imageSrc: "https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-transparent-svg-vector-bie-supply-2.png"
+    },
+    {
+        title: 'Gerenciando Estado no React com Redux',
+        category: 'Framework',
+        technology: 'JavaScript',
+        framework: 'Redux',
+        date: '2023-12-10',
+        slug: 'gerenciando-estado-react-redux',
+        imageSrc: "https://www.freepnglogos.com/uploads/logo-mysql-png/logo-mysql-mysql-logo-png-transparent-svg-vector-bie-supply-2.png"
+    }
     // Adicione mais posts conforme necessário
 ];
